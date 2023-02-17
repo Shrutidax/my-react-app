efa9afd937dd97b5d518b1cf0e6189585b18091a
Shrutidax <shruti.sahu@data-axle.com>
2023-02-17 15:31:19 +0530

    init

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,18 +5,22 @@ class Counter extends Component {
     state = {
         count : 0
     };
+
+    styles = {
+        color : 'blue'
+    }
     render() { 
         return (
-        <React.Fragment>
-            <span className="badge badge-primary">{this.formatCount()}</span>
-            <button>Increment</button>
-        </React.Fragment>
+        <div>
+            <span style = {this.styles} className='badge badge-pill badge-primary m-2'>{this.formatCount()}</span>
+            <button className='btn btn-secondary btn-sm'>Increment</button>
+        </div>
         );
     }
 
     formatCount() {
         const {count} = this.state;
-        return count === 0 ? Zero  : count;
+        return count === 0 ? "Zero"  : count;
     }
 }
  
