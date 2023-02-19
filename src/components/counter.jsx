c74a2996cc291a02a28c63221ce17e118f9968e3
Shrutidax <shruti.sahu@data-axle.com>
2023-02-19 14:28:20 +0530

    19/02 2:28

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,17 +3,30 @@ import React, { Component } from 'react';
 class Counter extends Component {
 
     state = {
-        count : 0
+        count : 0,
+        tags : ["tag1", "tag2" , "tag3"]
     };
 
     styles = {
         color : 'blue'
     }
+
+    // rendertags() {
+    //     if (this.state.tags.length === 0) 
+    //         return <p>There is no tags!</p>
+    //     return <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
+    // }
+
+    handleIncrement() {
+
+    }
+
     render() { 
         return (
         <div>
-            <span style = {this.styles} className='badge badge-pill badge-primary m-2'>{this.formatCount()}</span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+            <span style = {this.styles} className='badge badge-pill badge-primary m-2'>{this.formatCount()} </span>
+            <button onClick = {this.handleIncrement()} className = 'btn btn-secondary btn-sm'> Increment </button>
+            {/* {this.rendertags()} */}
         </div>
         );
     }
