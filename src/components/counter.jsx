91755a2b10ee9abe951998b166de7190367f3d0c
Shrutidax <shruti.sahu@data-axle.com>
2023-02-19 15:10:20 +0530

    updating state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,15 +17,21 @@ class Counter extends Component {
     //     return <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul>
     // }
 
-    handleIncrement() {
+    handleIncrement = () => {
+        this.setState({count : this.state.count + 1})
+    }
 
+    handleDecrement = () => {
+        if (this.state.count > 0)
+        this.setState({count : this.state.count - 1})
     }
 
     render() { 
         return (
         <div>
             <span style = {this.styles} className='badge badge-pill badge-primary m-2'>{this.formatCount()} </span>
-            <button onClick = {this.handleIncrement()} className = 'btn btn-secondary btn-sm'> Increment </button>
+            <button onClick = {this.handleIncrement} className = 'btn btn-secondary btn-sm'> + </button>
+            <button onClick = {this.handleDecrement} className = 'm-2 btn btn-secondary btn-sm'> - </button>
             {/* {this.rendertags()} */}
         </div>
         );
